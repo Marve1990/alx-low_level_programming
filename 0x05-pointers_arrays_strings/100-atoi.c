7f32fbe335fcaf22fcd88be5ad438cea41d788f8
Marve1990 <singermarvelous@gmail.com>
2022-09-22 13:17:16 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int _atoi(char *s)
 {
-	unsighned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 	while (*(s + count) != '\0')
 	{
@@ -22,6 +22,8 @@ int _atoi(char *s)
 				m *= 10;
 			size++;
 		}
+		count++;
+	}
 		for (i = count - size; i < count; i++)
 		{
 			oi = oi + ((*(s + i) - 48) * m);
